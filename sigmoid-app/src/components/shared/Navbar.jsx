8cf5020897d9f27688ac1c2255d4bdd7e447237f
Sevastian Bajenov <sevabaj@gmail.com>
2024-06-15 21:29:26 +0300

    Minor fix navbar.

--- sigmoid-app/src/components/shared/Navbar.jsx
+++ sigmoid-app/src/components/shared/Navbar.jsx
@@ -6,7 +6,6 @@ import "../../styles/Navbar.css";
 export default function Navbar() {
   const [value, setValue] = useState(-1);
   const navigate = useNavigate();
-  
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -26,61 +25,74 @@ export default function Navbar() {
   };
 
   return (
-<<<<<<< HEAD
-    <Box sx={{ position: "fixed", bottom: 0, width: "100%" }}>
-=======
-      <Box className="container" sx={{ position: "fixed", bottom: "0", width: "100%" }}>
->>>>>>> 11e90326bc709aa82af37f438e208872764a1bee
+    <Box
+      className="container"
+      sx={{ position: "fixed", bottom: "0", width: "100%" }}
+    >
       <BottomNavigation
         showLabels
         value={value}
         onChange={handleChange}
         sx={{ width: "100%" }}
       >
-        <BottomNavigationAction label="Forum" sx={{'& .MuiBottomNavigationAction-label': {
-              color: '#000',
-              fontSize: '20px',
+        <BottomNavigationAction
+          label="Forum"
+          sx={{
+            "& .MuiBottomNavigationAction-label": {
+              color: "#000",
+              fontSize: "20px",
             },
-            '&.Mui-selected': {
-              color: '#49a078',
-              fontSize: '20px',
+            "&.Mui-selected": {
+              color: "#49a078",
+              fontSize: "20px",
             },
-            '&:hover':{
-              '& .MuiBottomNavigationAction-label': {
-              color: '#49a078',
-              fontSize: '20px',
+            "&:hover": {
+              "& .MuiBottomNavigationAction-label": {
+                color: "#49a078",
+                fontSize: "20px",
+              },
             },
-            }}}/>
-        <BottomNavigationAction label="Tinder"  sx={{'& .MuiBottomNavigationAction-label': {
-              color: '#000',
-              fontSize: '20px',
+          }}
+        />
+        <BottomNavigationAction
+          label="Tinder"
+          sx={{
+            "& .MuiBottomNavigationAction-label": {
+              color: "#000",
+              fontSize: "20px",
             },
-            '&.Mui-selected': {
-              color: '#49a078',
-              fontSize: '20px',
+            "&.Mui-selected": {
+              color: "#49a078",
+              fontSize: "20px",
             },
-            '&:hover':{
-              '& .MuiBottomNavigationAction-label': {
-              color: '#49a078',
-              fontSize: '20px',
+            "&:hover": {
+              "& .MuiBottomNavigationAction-label": {
+                color: "#49a078",
+                fontSize: "20px",
+              },
             },
-            }}}/>
-        <BottomNavigationAction label="Matches" sx={{'& .MuiBottomNavigationAction-label': {
-              color: '#000',
-              fontSize: '20px',
+          }}
+        />
+        <BottomNavigationAction
+          label="Matches"
+          sx={{
+            "& .MuiBottomNavigationAction-label": {
+              color: "#000",
+              fontSize: "20px",
             },
-            '&.Mui-selected': {
-              color: '#49a078',
-              fontSize: '20px',
+            "&.Mui-selected": {
+              color: "#49a078",
+              fontSize: "20px",
             },
-            '&:hover':{
-              '& .MuiBottomNavigationAction-label': {
-              color: '#49a078',
-              fontSize: '20px',
+            "&:hover": {
+              "& .MuiBottomNavigationAction-label": {
+                color: "#49a078",
+                fontSize: "20px",
+              },
             },
-            }}}/>
+          }}
+        />
       </BottomNavigation>
     </Box>
-    
   );
 }
