9dfc96506f0a11bd7575a141cd6e5b97452d4735
micha <michael.echim@gmail.com>
2024-06-15 21:43:35 +0300

    tinders work!!!!!!!! (without backend :c)

---
+++ sigmoid-app/src/components/tinder/TinderPrefs.jsx
@@ -0,0 +1,127 @@
+import TinderCard from "react-tinder-card";
+import { useEffect, useState } from "react";
+import MyTinderCard from "../tinder/MyTinderCard";
+import { Button } from "@mui/material";
+const propertiesData = [
+    'Camera',
+    'Battery',
+    'Durability',
+    'Performance',
+    'Memory'
+]
+function pairEachElement(arr) {
+    let pairs = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j < arr.length; j++) {
+        pairs.push([arr[i], arr[j]]);
+      }
+    }
+
+    return pairs;
+  }
+
+function TinderPrefs() {
+  const [properties, setProperties] = useState(propertiesData);
+  const [propertyPairs, setPropertyPairs] = useState([]);
+  const [direction, setDirection] = useState('');
+
+  useEffect(() => {
+    setPropertyPairs(pairEachElement(properties));
+  }, []);
+
+  useEffect(() => {
+    console.log(properties);
+  },[properties])
+  const swiped = (direction, nameToDelete) => {
+    console.log('swiped!');
+    setDirection(direction.toString());
+    console.log(propertyPairs[propertyPairs.length-1]);
+    if (propertyPairs[0]){
+        let chosenIndex, loserIndex;
+
+        if (direction == 'left'){            
+            for (let i = 0; i < properties.length; i++){
+                if (properties[i] == propertyPairs[propertyPairs.length-1][0]){
+                    chosenIndex = i;
+                }
+                if (properties[i] == propertyPairs[propertyPairs.length-1][1]){
+                    loserIndex = i;
+                }
+            }
+            
+        } else if (direction == 'right'){
+            for (let i = 0; i < properties.length; i++){
+                if (properties[i] == propertyPairs[propertyPairs.length-1][1]){
+                    chosenIndex = i;
+                }
+                if (properties[i] == propertyPairs[propertyPairs.length-1][0]){
+                    loserIndex = i;
+                }
+            }  
+        }
+
+        let tempProps = properties;
+        console.log(direction + " " + chosenIndex + " " + loserIndex);
+        if (loserIndex < chosenIndex){
+            let temp = properties[loserIndex];
+            tempProps[loserIndex] = tempProps[chosenIndex];
+            tempProps[chosenIndex] = temp;
+        }
+        setProperties(tempProps);
+        console.log(properties);
+    }
+  };
+
+  const outOfFrame = async(name) => {
+    console.log(name + " left the screen!");
+    setPropertyPairs(propertyPairs => propertyPairs.filter(pair => pair.toString() !== name));
+  };
+  const tinderOffset = 350;
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: tinderOffset,
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+        justifyItems: "center",
+      }}
+    >
+        <h1  style={{ position: "absolute" ,top:-tinderOffset}}>Set your preferences</h1>
+        {/* <h2 style={{ position: "absolute" }}>
+            No more People Left
+        </h2> */}
+        {propertyPairs.map((pair) => (
+            <>
+                <TinderCard
+                    className="swipe"
+                    onSwipe={(dir) => swiped(dir, pair.toString())}
+                    key={pair.toString()}
+                    onCardLeftScreen={() => outOfFrame(pair.toString())}
+                    preventSwipe={["down", "up"]}
+                    swipeRequirementType="velocity"
+                    swipeThreshold={1}
+                    >
+                        <MyTinderCard type='prefs' prefs={pair}/>
+                </TinderCard>
+            </>
+        ))}
+        {propertyPairs[0] ? 
+            <div style={{position: 'relative', top: tinderOffset/1.2, width: '85%', display: 'flex', flexDirection: 'row', justifyContent:'space-between'}}>
+                <Button size='large' style={{color:'black', fontWeight: 'bold'}}>{propertyPairs[propertyPairs.length-1][0]}</Button>
+                <Button style={{color:'black', fontWeight: 'bold'}}>{propertyPairs[propertyPairs.length-1][1]}</Button>
+
+            </div>
+            : null
+        }
+    </div>
+  );
+}
+
+export default TinderPrefs;
