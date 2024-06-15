0a50aebf638e45223ae0e2c7e73c6d935d0d2df4
Sevastian Bajenov <sevabaj@gmail.com>
2024-06-15 17:09:46 +0300

    Add categories page.

--- sigmoid-app/src/routes/PageLayout.jsx
+++ sigmoid-app/src/routes/PageLayout.jsx
@@ -7,12 +7,12 @@ import Polls from "../pages/Polls";
 export default function PageLayout() {
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/tinder" element={<Tinder />} />
         <Route path="/polls" element={<Polls />} />
         <Route path="/matches" element={<Matches />} />
       </Routes>
+      <Navbar />
     </>
   );
 }
