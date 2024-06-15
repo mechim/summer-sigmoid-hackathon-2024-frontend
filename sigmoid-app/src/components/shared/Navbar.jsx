998f9db4aab3d76f7c759c7feb650a9236065c11
Sevastian Bajenov <sevabaj@gmail.com>
2024-06-15 13:33:17 +0300

    Add navbar.

---
+++ sigmoid-app/src/components/shared/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Box, BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    switch (newValue) {
+      case 0:
+        navigate("/polls");
+        break;
+      case 1:
+        navigate("/tinder");
+        break;
+      case 2:
+        navigate("/matches");
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Box sx={{ position: "fixed", bottom: "0", width: "100%" }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{ width: "100%" }}
+      >
+        <BottomNavigationAction label="Polls" />
+        <BottomNavigationAction label="Tinder" />
+        <BottomNavigationAction label="Matches" />
+      </BottomNavigation>
+    </Box>
+  );
+}
